f9ad3e76612564540a8a5f763f14d9fead2b0600
nightofknife <73384329+nightofknife@users.noreply.github.com>
2024-04-06 21:06:32 +0800

    Update trade_action.py

--- main/resource/function/trade_action.py
+++ main/resource/function/trade_action.py
@@ -1,62 +1,48 @@
 from airtest.core.api import *
 import json
 
-def replaceproduct(facility=""):
-    with open("resource/argument/loaction.json") as f:
-        data = json.load(f)
-    print(data["product"][facility])
-    return data["product"][facility]
 
+def test(type,list):
+    dealproduct(type, list)
 
-def buyproduct(product=None):
-    print("?")
+#
+def dealproduct(type=0, product=None):
+    """
+
+    :param type: 0是买 1是卖商品
+    :param product: 列表，目前只支持中文商品列表，后期会改成商品代号
+    :return: True表示正常完成
+    """
+    # 检查商品列表是否为空
     if product is None:
         product = []
         print("空参数")
+        return False
+
+    # 识别列表中所有商品，找到的点一下，
     newproduct = product.copy()
     while newproduct:
         for i in product:
             loc = exists(
-                Template(filename="resource/template/product/" + replaceproduct(i) + ".png", resolution=(1280, 720)))
+                Template(filename="resource/template/product/" + i + ".png", resolution=(1280, 720)))
             if loc:
                 touch((loc[0] + 120, loc[1]))
                 newproduct.remove(i)
         product = newproduct
         print("待购货物", product)
         swipe((670, 650), (670, 200), duration=1)
-    loc = exists(Template(filename="resource/template/action/buy_product.png", resolution=(1280, 720)))
-    if loc:
-        touch(loc)
-    flag = True
-    while flag:
-        try:
-            if wait(Template(filename="resource/template/guide/map_ui.png", resolution=(1280, 720)), timeout=1):
-                flag = False
-        except TargetNotFoundError:
-            touch(v=(20, 700))
 
-
-def sellproduct(product=None):
-    print("?")
-    if product is None:
-        product = []
-        print("空参数")
-    newproduct = product.copy()
-    while newproduct:
-        for i in product:
-            loc = exists(
-                Template(filename="resource/template/product/" + replaceproduct(i) + ".png", resolution=(1280, 720)))
-            if loc:
-                touch((loc[0] + 120, loc[1]))
-                print(i, loc)
-                newproduct.remove(i)
-        product = newproduct
-        print(product)
-        swipe((670, 650), (670, 200), duration=1)
-    loc = exists(Template(filename="resource/template/action/sell_product.png", resolution=(1280, 720)))
+    #     点击购买或售出按键
+    loc = exists(Template(
+        filename="resource/template/action/buy_product.png" if type == 0 else "resource/template/action/sell_product.png",
+        resolution=(1280, 720)))
     if loc:
         touch(loc)
 
+    sleep(2)
+    # 有时候会出问题，加个sleep看看
+
+    #     搜索回到主界面按键，没有说明报表还在，点击左下角，循环处理
     flag = True
     while flag:
         loc = exists(Template(filename="resource/template/guide/home.png", resolution=(1280, 720)))
@@ -65,4 +51,4 @@ def sellproduct(product=None):
             flag = False
         else:
             touch((20, 700))
-    print("end")
+    return True
