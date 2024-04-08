19fdf15af588dd96d3730123ee89da4f3730cb2e
sjfhsjfh <sjfhsjfh@qq.com>
2024-04-09 01:22:39 +0800

    chore: add helper

--- test.py
+++ test.py
@@ -5,14 +5,24 @@ class TestPriceDatabase(unittest.TestCase):
     """价格相关功能测试"""
     
     def setUp(self) -> None:
-        from main.database import GameData
+        from main.database import GameData, CITIES
         from main.database.get_price import get_price_table
         self.GameData = GameData
+        self.CITIES = CITIES
         self.get_price_table = get_price_table
         
     def test_load_price(self):
         """测试爬取价格表"""
         self.get_price_table()
+        
+    def test_faatigue_cost(self):
+        """测试疲劳值计算"""
+        for city in self.CITIES:
+            for other_city in self.CITIES:
+                if city == other_city:
+                    continue
+                fatigue_cost = city.travel_cost(other_city)
+                print(f"{city} -> {other_city} 疲劳消耗: {fatigue_cost}")
     
     def tearDown(self) -> None:
         # importlib.reload(main.database)
