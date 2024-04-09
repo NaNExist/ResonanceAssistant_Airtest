465031b8ef8fc2d2662f54bc583c5c4c157348a4
nightofknife <73384329+nightofknife@users.noreply.github.com>
2024-04-09 22:57:17 +0800

    Update core.py

--- main/resource/function/core.py
+++ main/resource/function/core.py
@@ -16,7 +16,7 @@ def program_plan():
     pass
 #占位使用
 
-def autorun(citylist, productlist1, productlist2,city1book = 0,city2book = 0):
+def autorun(citylist, productlist1, productlist2,city1book = 0,city2book = 0,times =-1):
     """
 
     :param citylist: 往返城市列表
@@ -24,12 +24,13 @@ def autorun(citylist, productlist1, productlist2,city1book = 0,city2book = 0):
     :param productlist2: 2号城市商品购买列表
     :param city1book: 购买1号城市商品用多少书
     :param city2book: 购买2号城市商品用多少书
+    :param times:往返多少次
     :return:
     """
     # 预处理数据
-    citydir = {"阿妮塔能源研究站": "a", "7号自由港": "7", "澄明数据中心": "c", "修格里城": "x", "铁盟哨站": "tie",
+    citydir = {"阿妮塔能源研究所": "a", "7号自由港": "7", "七号自由港": "7","澄明数据中心": "c", "修格里城": "x", "铁盟哨站": "tie",
                "荒原站": "h", "曼德矿场": "m", "淘金乐园": "t", "阿妮塔战备工厂": "an"}
-    citydir2 = {"阿妮塔能源研究站": "anita_energy_research_institute", "7号自由港": "freeport",
+    citydir2 = {"阿妮塔能源研究所": "anita_energy_research_institute", "7号自由港": "freeport", "七号自由港": "freeport",
                 "澄明数据中心": "clarity_data_center_administration_bureau",
                 "修格里城": "shoggolith_city", "铁盟哨站": "brcl_outpost",
                 "荒原站": "wilderness_station", "曼德矿场": "mander_mine", "淘金乐园": "onederland"}
@@ -41,9 +42,9 @@ def autorun(citylist, productlist1, productlist2,city1book = 0,city2book = 0):
 
     # 不在1号城的前往1号城
     if cityname != citydir2[citylist[0]]:
-        travel.test(citydir[citylist[0]])
+        travel.citytravel(citydir[citylist[0]])
 
-    while True:
+    while times!=0:
         # 这里在1号城
         cityname = citydir2[citylist[0]]
         print(cityname)
@@ -52,7 +53,7 @@ def autorun(citylist, productlist1, productlist2,city1book = 0,city2book = 0):
         guide.enterexchange(cityname=cityname)
         trade.test(productlist2, productlist1, buybook=city1book)
 
-        travel.test(citydir[citylist[1]])
+        travel.citytravel(citydir[citylist[1]])
 
         # 这里在2号城
         cityname = citydir2[citylist[1]]
@@ -62,5 +63,7 @@ def autorun(citylist, productlist1, productlist2,city1book = 0,city2book = 0):
         guide.enterexchange(cityname=cityname)
         trade.test(productlist1, productlist2, buybook=city2book)
 
-        travel.test(citydir[citylist[0]])
+        travel.citytravel(citydir[citylist[0]])
+
+        times-=1
 
